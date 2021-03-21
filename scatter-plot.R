5032e528364b7d3727f06748725f564fdde66576
vibix10 <vibix10@gmail.com>
2021-03-21 12:44:17 +0200

    commented code.

--- scatter-plot.R
+++ scatter-plot.R
@@ -3,3 +3,4 @@ library(tidyverse)
 data("murders")
 murders %>% ggplot(aes(state, total, color = region)) + 
   geom_point() + theme(axis.text.x = element_text(angle = 90))
+# add the theme for tilting states
